e332781c2c00640723828d3af67413c4c7a7e908
Lukman-01 <105321679+Lukman-01@users.noreply.github.com>
2022-07-28 09:36:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let username, userGuess;
+
+let range = 2; 
+let stage = 1; 
+let point = 0;
+
+const generate = (range) => {
+  return Math.random() * (range) + 1;
+};
+
+const welcome = () => {
+  username = prompt("Enter your username: ");
+  
+  console.log("\nHi " + username + ", Welcome to the number guessing game!");
+  userGuess = parseInt(prompt("\nEnter your guess number: "));
+};
+
+welcome();
+
+while(true){
+  let randomNumber = parseInt(generate(range));
+  
+  if (userGuess === randomNumber){
+    
+    range++;
+    stage++;
+    point++;
+
+    console.log("\n You guessed the number right! \n");
+    console.log("The random number generated is: ",randomNumber);
+    console.log("\nYou nw have ",point,"point");
+    
+    userGuess = parseInt(prompt("\nHey user you have now moved to stage " + stage + ", guess another number: "));
+    
+  }else{
+    console.log("\nGame over. you guessed incorrectly.\n\nThe number is: " ,randomNumber);
+    break;
+  }
+}
